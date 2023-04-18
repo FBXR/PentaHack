81ab3b620bee4426ac3271e04f614944bd9ec1f3
FBXR <128603256+FBXR@users.noreply.github.com>
2023-04-18 22:40:44 +0700

    Add files via upload

---
+++ PentaHack.py
@@ -0,0 +1,15 @@
+﻿import time
+import random 
+
+pers = 0
+
+while pers <= 100:
+
+    timer = '{0}'.format(pers)
+    print(f"The Pentagon is hacked to - {pers}%", end = '\r')
+    time.sleep(random.uniform(0.1, 0.7))
+    pers += random.randint(1, 5 )
+    
+print('The Pentagon has been successfully hacked! ')
+
+time.sleep(3)
